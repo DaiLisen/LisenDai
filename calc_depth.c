4f5ee135086961451dac8cb2134cdfebd7597a88
DaiLisen <42723389+DaiLisen@users.noreply.github.com>
2018-09-20 21:54:00 -0700

    Add files via upload

--- calc_depth.c
+++ calc_depth.c
@@ -1,5 +1,5 @@
 /*
- * Project 1-1: Computing a Displacement Map
+ * PROJ1-1: YOUR TASK A CODE HERE
  *
  * Feel free to define additional helper functions.
  */
@@ -12,108 +12,144 @@
 
 /* Implements the normalized displacement function */
 unsigned char normalized_displacement(int dx, int dy,
-                                      int maximum_displacement) {
-    
+        int maximum_displacement) {
+
     double squared_displacement = dx * dx + dy * dy;
     double normalized_displacement = round(255 * sqrt(squared_displacement) / sqrt(2 * maximum_displacement * maximum_displacement));
     return (unsigned char) normalized_displacement;
-    
+
 }
 
-int squared_euclidean(int width, int height, unsigned char *a, unsigned char *b) {
-    int d = 0;
-    int counter = 0;
-    int size = (2 * width + 1) * (2 * height + 1);
-    while(counter++ < size){
-        d += (*a++ - *b++)^2;
-    }
-    return d;
-}
-
-int low_bondary(int x, int maximum_displacement){
-    int x_min;
-    if(x < maximum_displacement)
-        x_min = 0;
-    else
-        x_min = x - maximum_displacement;
-    return x_min;
-}
-
-int high_bondary(int x, int maximum_displacement, int image_width){
-    int x_max;
-    if (x + maximum_displacement > image_width - 1) {
-        x_max = image_width - 1;
-    }
-    else{
-        x_max = x + maximum_displacement;
-    }
-    return x_max;
+int squared_euclidean(int feature_width, int feature_height, 
+		unsigned char *left_feature, unsigned char *right_feature) {
+	int distance = 0;
+	int current_length = 0;
+	int feature_length = (2 * feature_width + 1) * (2 * feature_height + 1);
+	while(current_length < feature_length){
+		int diff = (*left_feature - *right_feature);
+		left_feature++; right_feature++;
+		distance += diff * diff;
+		current_length++;
+	}
+	return distance;
 }
 
 
-
-void depth_finder(int x, int y, int x_min, int x_max, int y_min, int y_max, unsigned char *depth_map, unsigned char *left, unsigned char *right, int image_width, int image_height, int feature_width, int feature_height,int maximum_displacement){
-    int x0 = -1;
-    int y0 = -1;
-    int sum = 0;
-    int h = - feature_height;
-    int w = - feature_width;
-    unsigned int euclidean = -1;
-    while(y_min++ <= y_max){
-        while(x_min++ <= x_max){
-            if (x_min < feature_width || y_min - feature_height < 0 || x_min + feature_width > image_width - 1 || y_min + feature_height > image_height - 1) {
-                continue;
-            }
-            while(h++ <= feature_height) {
-                while(w++ <= feature_width) {
-                    sum += (left[(y + h) * image_width + (x + w)] - right[(y_min + w) * image_width + (x_min + w)]) * (left[(y + h) * image_width + (x + w)] - right[(y_min + w) * image_width + (x_min + w)]);
-                }
-            }
-            if (sum < euclidean) {
-                euclidean = sum;
-                x0 = x_min - 1;
-                y0 = y_min - 1;
-            }
-            else if (sum == euclidean) {
-                if (normalized_displacement(x - x_min, y - y_min, maximum_displacement) < normalized_displacement(x - y0, y - y0, maximum_displacement)) {
-                    y0 = y_min;
-                    x0 = x_min;
-                }
-            }
-        }
-    }
-    *depth_map = normalized_displacement(x - x0, y - y0, maximum_displacement);
+int Two2OneD(int x, int y, int width){
+	return width * y + x;
 }
 
+void extract_box(unsigned char *box, unsigned char *pic, int searchX, int searchY, int box_width, int box_height, int pic_width, int pic_height) {
+	int curr_size = 0, colI = 0;
+	int currX = searchX - (box_width - 1)/2;
+	int currY = searchY - (box_height - 1)/2; 
+	int size = box_width * box_height;
+	unsigned char *original_pic = pic;
+	pic +=  Two2OneD(currX, currY, pic_width);
+	for( ; curr_size < size ; curr_size++){
+		if( colI  >  box_width - 1){
+			colI  = 0;
+			//pic += (pic_width - currX) + searchX + 1;  
+			currX = searchX - (box_width - 1)/2; 
+			currY++;
+			pic = original_pic + Two2OneD(currX, currY, pic_width);
+		}			
+		if( (currX < 0) | (currY < 0) | (currY > pic_height)) {
+			*box = 0;
+		}
+		*box = *pic;
+		colI++; currX++; pic++; box++;
+	}
+}
 
-
-
+// x and y are coords of  pixels of center of search space 
+unsigned char scanFeatureRight(unsigned char *left_feature, unsigned char *right_image, int right_image_width, int right_image_height, int feature_width, int feature_height, int x, int y, int max_displacement){
+	int curr_feature = 0;
+	int max_features = (2 * max_displacement + 1) * (2 * max_displacement + 1);
+	int startX = x - max_displacement;
+	int startY = y - max_displacement; 
+	int matchX; int matchY; int distance;
+	int smallestDistance = 0;
+	unsigned char smallestNorm = 0;
+	unsigned char currNorm;
+	int enter = 1;
+	unsigned char testNORM = 0;
+	unsigned char right_feature[(2 * feature_width + 1) * (2*feature_height+1)];
+	for( ; curr_feature < max_features; curr_feature++){ 
+		if( startX > x + (max_displacement)){
+			startX = x - max_displacement;
+			startY++;
+		}
+		if( (startX  - feature_width < 0) | (startY > right_image_height)) {
+			startX++;
+			continue;
+		}
+		if( ((startX + feature_width) > right_image_width - 1) | ((startY + feature_height) > right_image_height - 1)){
+			startX++;
+			continue;
+		}  
+		testNORM =  normalized_displacement(x - startX, y - startY, max_displacement); 
+		extract_box(right_feature, right_image, startX, startY, 2 * feature_width + 1,
+			 2 * feature_height + 1,  right_image_width, right_image_height);
+	 	distance = squared_euclidean(feature_width, feature_height, left_feature, right_feature);
+		if( startY - feature_height < 0 ){
+			startX++;
+			continue;
+		}
+		if( (enter) | (distance < smallestDistance)){
+			smallestDistance = distance;
+			matchX = startX; matchY = startY;
+			smallestNorm = normalized_displacement(x - matchX, y - matchY, max_displacement);
+			enter = 0;
+		 } else if(distance == smallestDistance){
+			currNorm = normalized_displacement(x - startX, y - startY, max_displacement); 
+			if ( currNorm < smallestNorm ){
+				matchX = startX; matchY = startY;
+				smallestNorm = currNorm;
+		 	}
+		 }
+		startX++;	
+	}
+	return normalized_displacement(x - matchX, y - matchY, max_displacement);
+}
 void calc_depth(unsigned char *depth_map, unsigned char *left,
-                unsigned char *right, int image_width, int image_height,
-                int feature_width, int feature_height, int maximum_displacement) {
-    int x = 0;
-    int y = 0;
-    int x_max = 0;
-    int x_min = 0;
-    int y_max = 0;
-    int y_min = 0;
-    int i;
-    int image_size = image_width * image_width;
-    for(i = 0; i < image_size; i++){
-        y = i / image_width;
-        x = i % image_width; // (y, x)
-        if (maximum_displacement == 0)
-            *depth_map = 0;
-        else if (x < feature_width || y < feature_height || x + feature_width >= image_width || y + feature_height >= image_width)
-            *depth_map = 0;
-        else{
-            x_min = low_bondary(x, maximum_displacement);
-            y_min = low_bondary(y, maximum_displacement);
-            x_max = high_bondary(x, maximum_displacement, image_width);
-            y_max = high_bondary(y, maximum_displacement, image_width);
-        }
-        depth_finder(x, y, x_min, x_max, y_min, y_max, depth_map, left, right, image_width, image_height,  feature_width, feature_height, maximum_displacement);
-        depth_map++;
-    }
-    /* YOUR CODE HERE */
+        unsigned char *right, int image_width, int image_height,
+        int feature_width, int feature_height, int maximum_displacement) {
+	//Variables to track the size of dimensions
+	//-----------------------------------------
+	//  x * y of the feature
+	int feature_box_width = 2 * feature_width + 1;
+	int feature_box_height = 2 * feature_height + 1;
+	int	image_size = image_width * image_height;
+	int x = 0, y = 0, curr_size = 0;
+	// for (int i = 0; i < image_size; i++){
+	// 	printf("%d", left[i]);
+	// }
+	// printf("END OF IMAGE\n\n\n");	
+	for( ; curr_size < image_size; curr_size++){
+		if( x > image_width - 1){
+			y++;
+			x = 0;
+		}
+		if( ((x - feature_width) < 0) | ((y - feature_height) < 0) ) {
+			*depth_map = 0;
+		}
+		else if( ( (x + feature_width) > image_width - 1 ) | ((y + feature_height) > image_height - 1)){		
+			*depth_map = 0;
+		}
+		else if( ( (y - feature_height) < 0 )){
+			*depth_map = 0;
+		}
+		else{  
+			unsigned char leftFeature[feature_box_width * feature_box_height];
+			extract_box(leftFeature, left, x, y, feature_box_width, feature_box_height,
+					image_width,  image_height);
+			unsigned char norm_distance = scanFeatureRight(leftFeature, right, image_width, image_height, feature_width, feature_height, x, y, maximum_displacement);
+			*depth_map = norm_distance;
+		}
+		x++; depth_map++;
+	}	
+
 }
+
+
